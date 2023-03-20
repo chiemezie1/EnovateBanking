eb8685cef4076f53eb931389e1673ec0bb94f1d7
Replit user <>
2023-03-20 14:05:28 +0000

    init the project and added hardhar

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+    deps = [
+        pkgs.nodejs-16_x
+        pkgs.nodejs-16_x
+        pkgs.nodejs-16_x
+        pkgs.import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+        pkgs.import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+        pkgs.cowsay
+    ];
+}
